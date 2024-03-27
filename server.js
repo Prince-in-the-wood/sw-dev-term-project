f41962fe7d4da47deba6e5be881c468f8053381f
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-03-27 18:05:22 +0700

    Init express setup and folder structure

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Route file
+// const reservation = require('./routes/reservations');
+// const restaurant = require('./routes/restaurants');
+// const auth = require('./routes/auth');
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+
+const app = express();
+
+//Body parser
+app.use(express.json());
+
+//Mount routers
+// app.use('./api/v1/reservations', reservation);
+// app.use('./api/v1/restaurant', restaurant);
+// app.use('./api/v1/auth', auth);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
