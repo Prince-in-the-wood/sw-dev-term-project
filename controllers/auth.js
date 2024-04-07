b871d0af528ad937dbacc75e6fa9ac7c53a27b34
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-04-07 10:33:04 +0700

    feat: Add protect route and authorize

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,3 +61,11 @@ exports.login = async (req, res, next) => {
     }
 
 }
+
+exports.getMe = async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+}
