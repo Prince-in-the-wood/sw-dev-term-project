b9cb9436a9246ef6ea2754f5c70b17650a65c0df
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-04-07 10:00:37 +0700

    feat: Add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,3 +19,31 @@ exports.register = async (req, res, next) => {
         res.status(400).json({ success: false });
     }
 }
+
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ success: false, msg: 'Please provide an email and password' });
+    }
+
+    try {
+        const user = await User.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(400).json({ success: false, msg: 'Invalid credentials' });
+        }
+
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ success: false, msg: 'Invalid credentials' });
+        }
+
+        const token = user.getSignedJwtToken();
+
+        res.status(200).json({ success: true, token });
+    } catch (error) {
+        res.status(400).json({ success: false, msg: "Cannot convert email or password to string" })
+    }
+
+}
